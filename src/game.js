96afce27c55f2807f848da831eeea9f8d80c9c3e
Bruno <mitozgamer137@gmail.com>
2022-09-21 18:52:13 -0300

    split code into two files

---
+++ src/game.js
@@ -0,0 +1,119 @@
+import { config } from "./config.js";
+
+export const gameState = {
+    myId: null,
+    players:{},
+    fruits: {}
+}
+
+
+export function movePlayer({ playerId, move }) {
+    const acceptedMoves = {
+        down() {
+            if(gameState.players[playerId].positionY + 1 === config.screenHeight) return
+            gameState.players[playerId].positionY++
+        },
+        up() {
+            if(gameState.players[playerId].positionY === 0) return
+            gameState.players[playerId].positionY--
+        },
+        left() {
+            if(gameState.players[playerId].positionX === 0) return
+            gameState.players[playerId].positionX--
+        },
+        right() {
+            if(gameState.players[playerId].positionX + 1 === config.screenWidth) return
+            gameState.players[playerId].positionX++
+        },
+    }
+
+    const moveFunction = acceptedMoves[move];
+
+    if(!moveFunction) return;
+
+    moveFunction();
+    checkPlayerCollision({ playerId })
+    renderScreen();
+}
+
+
+function checkPlayerCollision({ playerId }) {
+    const player = gameState.players[playerId];
+
+    for(const fruitId in gameState.fruits){
+        const fruit = gameState.fruits[fruitId];
+        if(player.positionX !== fruit.positionX || player.positionY !== fruit.positionY) continue
+
+        removeFruit({
+            fruitId
+        })
+    }
+}
+
+
+export function addPlayer({ playerId, positionX, positionY }) {
+    gameState.players[playerId] = {
+        positionX,
+        positionY
+    }
+    
+    renderScreen();
+}
+
+export function addFruit({ fruitId, positionX, positionY }) {
+    gameState.fruits[fruitId] = {
+        positionX,
+        positionY
+    }
+
+    renderScreen();
+}
+
+
+export function removePlayer({ playerId }) {
+    delete gameState.players[playerId];
+    
+    renderScreen();
+}
+
+export function removeFruit({ fruitId }) {
+    delete gameState.fruits[fruitId];
+
+    renderScreen();
+}
+
+
+function renderScreen() {
+    const canvas = Array.from({
+        length: config.screenHeight 
+    }, () => Array(config.screenWidth).fill('\x1b[47m  '));
+
+    canvas[config.screenHeight-1][config.screenWidth-1] = '\x1b[47m  \x1b[0m'
+
+
+    const players = gameState.players;
+    const fruits = gameState.fruits;
+
+    for(const prop in players){
+        canvas[players[prop].positionY][players[prop].positionX] = '\x1b[41m  \x1b[0m'
+    }
+    for(const prop in fruits){
+        canvas[fruits[prop].positionY][fruits[prop].positionX] = '\x1b[42m  \x1b[0m'
+    }
+
+    let output = '\n\n\x1b[0m  ';
+
+    canvas.map(line => {
+        line.map(field => {
+            output += field
+        })
+        output += '\n\x1b[0m  '
+    })
+    
+    console.clear();
+    console.log(output);
+
+    
+}
+
+renderScreen();
--- src/main.js
+++ src/main.js
@@ -1,14 +1,7 @@
 import readline from 'readline';
 import { io } from 'socket.io-client';
 
-import { config } from "./config.js";
-
-const gameState = {
-    myId: null,
-    players:{},
-    fruits: {}
-}
-
+import { addPlayer, gameState, movePlayer } from './game.js';
 
 const socket = io('ws://localhost:3333');
 
@@ -24,118 +17,6 @@ socket.on('initialPositions', positions => {
 socket.on('newPlayer', player => {
     addPlayer({ playerId: player.playerId, positionX: player.positionX, positionY: player.positionY });
 }) 
-
-function movePlayer({ playerId, move }) {
-    const acceptedMoves = {
-        down() {
-            if(gameState.players[playerId].positionY + 1 === config.screenHeight) return
-            gameState.players[playerId].positionY++
-        },
-        up() {
-            if(gameState.players[playerId].positionY === 0) return
-            gameState.players[playerId].positionY--
-        },
-        left() {
-            if(gameState.players[playerId].positionX === 0) return
-            gameState.players[playerId].positionX--
-        },
-        right() {
-            if(gameState.players[playerId].positionX + 1 === config.screenWidth) return
-            gameState.players[playerId].positionX++
-        },
-    }
-
-    const moveFunction = acceptedMoves[move];
-
-    if(!moveFunction) return;
-
-    moveFunction();
-    checkPlayerCollision({ playerId })
-    renderScreen();
-}
-
-
-function checkPlayerCollision({ playerId }) {
-    const player = gameState.players[playerId];
-
-    for(const fruitId in gameState.fruits){
-        const fruit = gameState.fruits[fruitId];
-        if(player.positionX !== fruit.positionX || player.positionY !== fruit.positionY) continue
-
-        removeFruit({
-            fruitId
-        })
-    }
-}
-
-
-function addPlayer({ playerId, positionX, positionY }) {
-    gameState.players[playerId] = {
-        positionX,
-        positionY
-    }
-    
-    renderScreen();
-}
-
-function addFruit({ fruitId, positionX, positionY }) {
-    gameState.fruits[fruitId] = {
-        positionX,
-        positionY
-    }
-
-    renderScreen();
-}
-
-
-function removePlayer({ playerId }) {
-    delete gameState.players[playerId];
-    
-    renderScreen();
-}
-
-function removeFruit({ fruitId }) {
-    delete gameState.fruits[fruitId];
-
-    renderScreen();
-}
-
-
-function renderScreen() {
-    const canvas = Array.from({
-        length: config.screenHeight 
-    }, () => Array(config.screenWidth).fill('\x1b[47m  '));
-
-    canvas[config.screenHeight-1][config.screenWidth-1] = '\x1b[47m  \x1b[0m'
-
-
-    const players = gameState.players;
-    const fruits = gameState.fruits;
-
-    for(const prop in players){
-        canvas[players[prop].positionY][players[prop].positionX] = '\x1b[41m  \x1b[0m'
-    }
-    for(const prop in fruits){
-        canvas[fruits[prop].positionY][fruits[prop].positionX] = '\x1b[42m  \x1b[0m'
-    }
-
-    let output = '\n\n\x1b[0m  ';
-
-    canvas.map(line => {
-        line.map(field => {
-            output += field
-        })
-        output += '\n\x1b[0m  '
-    })
-    
-    console.clear();
-    console.log(output);
-
-    
-}
-
-renderScreen();
-
 
 readline.emitKeypressEvents(process.stdin);
 process.stdin.setRawMode(true);
